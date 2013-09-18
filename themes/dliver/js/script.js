4aa464ce8b41364ea35e68c3faadb8eb42defb08
Stephan Zeidler <stephanzeidler@web.de>
2013-09-18 15:44:51 +0200

    add font + togglelist

---
+++ themes/dliver/js/script.js
@@ -0,0 +1,25 @@
+(function ($, Drupal, window, document, undefined) {
+
+  Drupal.behaviors.accordion = {
+    attach: function (context, settings) {
+      // hide all togglelist by default
+      $('.field-name-body h2').each(function () {
+        $(this).addClass('toggle-headline');
+        $(this).addClass('toggle-headline-closed');
+        $(this).next().hide().addClass('toggle-content');
+      });
+
+      // togglelist event
+      $('.field-name-body h2').toggle(function () {
+        $(this).removeClass('toggle-headline-closed');
+        $(this).addClass('toggle-headline-open');
+        $(this).next().show('slideDown');
+      }, function () {
+        $(this).removeClass('toggle-headline-open');
+        $(this).addClass('toggle-headline-closed');
+        $(this).next().hide('slideUp');
+      });
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
