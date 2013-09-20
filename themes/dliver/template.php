a6513bf24767d310cc097c3b21f65c8875031782
Stephan Zeidler <stephanzeidler@web.de>
2013-09-20 11:21:27 +0200

    shortened date when the start month = end month

--- themes/dliver/template.php
+++ themes/dliver/template.php
@@ -10,3 +10,27 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+/**
+ * Returns HTML for a date element formatted as a range.
+ */
+function dliver_date_display_range($variables) {
+  $date1 = $variables['date1'];
+  $date2 = $variables['date2'];
+  $timezone = $variables['timezone'];
+  $attributes_start = $variables['attributes_start'];
+  $attributes_end = $variables['attributes_end'];
+  if ($variables['dates']['format'] == 'jS F Y') {
+    $startMonth = substr($variables['dates']['value']['formatted_iso'], 0, 7);
+    $endMonth = substr($variables['dates']['value2']['formatted_iso'], 0, 7);
+    if ($startMonth == $endMonth) {
+      $date1 = substr($date1, 0, 4);
+    }
+  }
+
+  // Wrap the result with the attributes.
+  return t('!start-date - !end-date', array(
+    '!start-date' => '<span class="date-display-start"' . drupal_attributes($attributes_start) . '>' . $date1 . '</span>',
+    '!end-date' => '<span class="date-display-end"' . drupal_attributes($attributes_end) . '>' . $date2 . $timezone . '</span>',
+  ));
+}
