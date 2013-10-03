0d93b7c9ebefe88b602130214eedd25fa2cc5479
Stephan Zeidler <stephanzeidler@web.de>
2013-10-03 12:59:59 +0200

    fix design issues 20131001

---
+++ themes/dliver/templates/zone--preface.tpl.php
@@ -0,0 +1,7 @@
+<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>
+  <div<?php print $content_attributes; ?>>
+    <div class="zone-preface-inner">
+      <?php print $content; ?>
+    </div>
+  </div>
+<?php if ($wrapper): ?></div><?php endif; ?>
